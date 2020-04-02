769ef3f145081c23dd74a3cde92af3f5567651c8
HocineFEDANI <hocinefedani@gmail.com>
2020-04-02 21:12:16 +0100

    ajouter le fichier index.php

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Acceuil</title>
+</head>
+<body>
+
+<header>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+<script type="text/javascript" src="./js/jQuery.js"></script>
+<script src="./js/jsscript.js"></script>
+
+
+    <img src="./images/logo.png" alt="logo" class="logo">
+    
+ 
+    <div class="container_rechercher">
+        <input type="text" name="rechercher" class="recherche" placeholder="Rechercher">
+        <button class="btn_recherche"><i class="fas fa-search"></i></button>
+    </div>
+
+    <div class="inscription_connexion">
+      <a href="./admin"class="inscription" >Administrateur</a>
+      <a href="./pages/inscription.php" class="inscription">Crée un compte</a>
+      <a href="./pages/connexion.php" class="inscription">Se connecter</a>
+    </div>
+<nav>
+    
+    <ul class="navigation">
+    <a href="./pages/Immobilier.php" class="onglet_menu"><li>Immobiliers</li></a>
+    <a href=""><li>Déposer une annonce</li></a>
+    <a href="./pages/contact" class="onglet_menu"><li>Contact</li></a>
+    </ul>
+</nav>
+</header>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p>eeeeeeeeeeeeaaaaaaaaaaaaaaaaaaaa</p>
+<p></p>
+<p></p>
+<p>eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</p>
+<p></p>
+
+
+
+
+
+<footer>
+<div class="a_propos">
+    <h5 style="position:absolute;color:#ffffff">A PROPOS DE NOUS :</h5>
+</div>
+
+<div class="social-button">
+    <a href=""><i class="fab fa-facebook-f"></i></a>
+    <a href=""><i class="fab fa-twitter"></i></a>
+    <a href=""><i class="fab fa-instagram"></i></a>
+    <a href=""><i class="fab fa-linkedin"></i></a>
+    <a href=""><i class="fab fa-youtube"></i></a>
+</div>
+</div>
+</footer>
+</body>
+</html>
