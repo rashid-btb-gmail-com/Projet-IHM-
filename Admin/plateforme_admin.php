d96fcd86d69969a880f8c3f13f5996901f0b658e
HocineFEDANI <hocinefedani@gmail.com>
2020-04-10 23:39:08 +0100

    Ajout de la fonction Ajouter un bien en php

--- Admin/plateforme_admin.php
+++ Admin/plateforme_admin.php
@@ -12,25 +12,28 @@
 <body>
     <?php
     session_start();
-        
+    $db=new PDO('mysql:host=localhost;dbname=vilavie','root','');  
         if(($_SESSION["nom_admin"]!="user")||($_SESSION["psw_admin"]!="1234")){
             header("location: ./index.php");
         }
     ?>
     
-    <!--********************************Menu administrateur **************************-->
+    <!--**************************************Menu administrateur **********************************-->
     <nav class="menu_admin">
         <input type="image" src="../images/user.png" alt="photo de profile" class="photo_profil">
         <h2 class="nom_admin">NOM Prenom </h2>
         <hr color="black">
-        <input type="button" value="Ajouter un bien" class="btn_admin">
-        <input type="button" value="Gerer les biens" class="btn_admin">
-        <input type="button" value="Supprimer des biens" class="btn_admin">
-        <input type="button" value="Confirmer les annonces" class="btn_admin">
+        <a href="?action=ajouter_bien" class="onglet_admin">Ajouter un bien</a>
+        <a href="?action=gerer_biens" class="onglet_admin">Gerer les biens</a>
+        <a href="?action=supprimer_bien" class="onglet_admin">Supprimer des biens</a>
+        <a href="?action=confirmer_annonces" class="onglet_admin" style="height:35px">Confirmer les annonces</a>
+        
+
+        
         <hr color="black">
-        <input type="button" value="Ajouter un RDV" class="btn_admin btn_rdv"  >
-        <input type="button" value="Gérer les RDV" class="btn_admin btn_rdv"  >
-        <input type="button" value="Confirmer les RDV" class="btn_admin btn_rdv"  >
+        <a href="" class="onglet_admin onglet_rdv">Ajouter un RDV</a>
+        <a href="" class="onglet_admin onglet_rdv">Gérer les RDV</a>
+        <a href="" class="onglet_admin onglet_rdv">Confirmer les RDV</a>
         </nav>
 
         <div class="container">
@@ -41,9 +44,181 @@
                 <a href="" class="icon_header" title="Déconnexion"><i class="fas fa-sign-out-alt"></i></a>
             </div>
             </header>
+        <!--*****************************************Page admin*******************************************-->
+            <div class="page_admin">
+                <?php
+                    if(isset($_GET["action"])){
+                        //***************************affichage du formulaire ajouter des biens ************  */
+                        if($_GET["action"]=="ajouter_bien"){
+                            ?>
 
-            <div class="rdv">
-                <h3 class="titre_plfrm">Mes rendez-vous :</h3>
+                            
+                            <form action="" method="post" class="form_admin">
+                                <h2>Ajouter un bien :</h2>
+                                <input type="text" name="titre" class="inp_insc" placeholder="Titre" require><br>
+                                <textarea name="description" cols="35"  rows="10" class="inp_insc" style="height:150px; resize: none;" placeholder="Description du bien" require></textarea><br>
+                                <!--*********   liste des dairas de tizi ouzou   **********-->
+                                <input list="daira" name="daira" class="inp_insc" placeholder="Daïra" require>
+                                    <datalist id="daira">
+                                        <option value="Ain El Hammam">   
+                                        <option value="Azazga">
+                                        <option value="Azeffoun ">
+                                        <option value="Beni Douala">
+                                        <option value="Beni Yenni">
+                                        <option value="Boghni">
+                                        <option value="Bouzguen">
+                                        <option value="Draâ Ben Khedda">
+                                        <option value="Draâ El Mizan">
+                                        <option value="Iferhounène">
+                                        <option value="Larbaâ Nath Irathen">
+                                        <option value="Mâatkas">
+                                        <option value="Makouda">
+                                        <option value="Mekla">
+                                        <option value="Ouacif">
+                                        <option value="Ouadhia">
+                                        <option value="Ouaguenoun">
+                                        <option value="Tigzirt ">
+                                        <option value="Tizi Gheniff">
+                                        <option value="Tizi Ouzou">
+                                        <option value=" Tizi Rached">
+                                    </datalist>
+                                    
+                                <!--*********   liste des communes de tizi ouzou   **********-->
+                                <input list="commune" name="commune" class="inp_insc" placeholder="Commune" require><br>
+                                    <datalist id="commune">
+                                        <option value="Abi Youcef">
+                                        <option value="Aghribs">
+                                        <option value="Agouni Gueghrane">
+                                        <option value="Ain El Hammam">
+                                        <option value="Aïn Zaouia">
+                                        <option value="Aït Aggouacha">
+                                        <option value="Aït Aïssa Mimoun">
+                                        <option value="Aït Bouaddou">
+                                        <option value="Aït Boumahdi">
+                                        <option value="Aït Chafâa">
+                                        <option value="Aït Khellili">
+                                        <option value="Aït Mahmoud">
+                                        <option value="Aït Ouacif">
+                                        <option value="Aït Oumalou">
+                                        <option value="Aït Toudert">
+                                        <option value="Aït Yahia">
+                                        <option value="Aït Yahia Moussa">
+                                        <option value="Aït Yenni">
+                                        <option value="Aït Zmenzer">
+                                        <option value="Akbil">
+                                        <option value="Akerrou">
+                                        <option value="Assi Youcef">
+                                        <option value="Ath Zikki">
+                                        <option value="Azazga">
+                                        <option value="Azeffoun">
+                                        <option value="Beni Aïssi">
+                                        <option value="Beni Douala">
+                                        <option value="Boghni">
+                                        <option value="Boudjima">
+                                        <option value="Bounouh">
+                                        <option value="Bouzeguène">
+                                        <option value="Draâ Ben Khedda">
+                                        <option value="Draâ El Mizan">
+                                        <option value="Freha">
+                                        <option value="Frikat">
+                                        <option value="Iboudraren">
+                                        <option value="Idjeur">
+                                        <option value="Iferhounène">
+                                        <option value="Ifigha">
+                                        <option value="Iflissen">
+                                        <option value="Illilten">
+                                        <option value="Illoula Oumalou">
+                                        <option value="Imkiren">
+                                        <option value="Imsouhel">
+                                        <option value="Irdjen">
+                                        <option value="Larbaâ Nath Irathen">
+                                        <option value="Mâatkas">
+                                        <option value="Makouda">
+                                        <option value="Mechtras">
+                                        <option value="Mekla">
+                                        <option value="Mizrana">
+                                        <option value="Ouadhia">
+                                        <option value="Ouaguenoun">
+                                        <option value="Sidi Namane">
+                                        <option value="Souamaâ">
+                                        <option value="Souk El Thenine">
+                                        <option value="Tadmaït">
+                                        <option value="Tigzirt">
+                                        <option value="Timizart">
+                                        <option value="Tirmitine">
+                                        <option value="Tizi Gheniff">
+                                        <option value="Tizi N'Tleta">
+                                        <option value="Tizi Ouzou">
+                                        <option value="Tizi Rached">
+                                        <option value="Yakouren">
+                                        <option value="Yatafen">
+                                        <option value="Zekri">
+                                    
+                                    </datalist>
+
+
+                                <input type="number" name="surface" class="inp_insc" placeholder="Surface">
+                                <input type="number" name="nbr_etages" class="inp_insc" placeholder="Nombre d'étages">
+                                <input type="file" name="image_annonce"  ><br>
+                                <input type="submit" value="Envoyer" class="btn_inscr" name="submit">
+                                    <?php
+                                        if(isset($_POST["submit"])){
+                                            //pas d'image pour l'instant-------------
+                                            
+                                            
+                                            //   insertion dans la base de donnee
+                                            $insert=$db->prepare('INSERT INTO biens VALUES(NULL,?,?,?,?,?,?)');
+                                            $insert->execute(array($_POST["titre"],$_POST["description"],$_POST["daira"],$_POST["commune"],$_POST["surface"],$_POST["nbr_etages"]));
+                                            
+                                        }
+
+
+                                    ?>
+
+                            </form>
+                            
+                            
+                            <?php
+                        }
+                        if($_GET["action"]=="gerer_biens"){
+                            $select=$db->query('SELECT * FROM biens')
+                            ?>
+                            <div class="form_admin">
+                            <table class="liste_biens" cellpadding="3" rules="all">
+                                <colgroup span="6" class="columns"></colgroup>
+                                <tr>
+                                    <th>Titre</th>
+                                    <th>Surface</th>
+                                    <th>Etages</th>
+                                    <th>Daïra</th>
+                                    <th>Commune</th>
+                                    <th>Action</th>
+                                </tr>
+                                <?php
+                                while($donnees=$select->fetch()){
+
+                                    ?>
+                                    <tr>
+                                    <td><?php echo $donnees["titre"] ?></td>
+                                    <td><?php echo $donnees["surface"] ?></td>
+                                    <td><?php echo $donnees["etage"] ?></td>
+                                    <td><?php echo $donnees["daira"] ?></td>
+                                    <td><?php echo $donnees["commune"] ?></td>
+                                    <td><a href="">Modifier</a>   <a href="">Supprimer</a></td>
+                                    </tr>
+
+                                    <?php
+                                }
+
+                                ?>
+                                
+                                
+                            </table>
+                            </div>
+                            <?php
+                        }
+                    }
+                ?>
 
                 
             </div>
