d2bc822e731bc9fff2120ce58d26a39b51be5e47
HocineFEDANI <hocinefedani@gmail.com>
2020-04-16 20:27:51 +0100

    resolution des problemes de controle js

--- js/js.js
+++ js/js.js
@@ -1,38 +1,115 @@
 function verifier_champs(){
+    const vert='<i class="fas fa-check" style="color:green;font-size:23px;"></i>';
+    const rouge='<i class="fas fa-times" style="color:red;font-size:23px;"></i>';
     var i;//indice i pour la boucle for
-    // verifier les deux mot de passe pour confirmer
-    var mdp=document.getElementById("mdp").value;
-    var mdp2=document.getElementById("mdp2").value;
-    if(mdp!=mdp2){
-        document.getElementById("msg").innerHTML= "<p style='color:red'>Veuillez saisir à nouveau le mot de passe .</p>"; 
-    }
-    else document.getElementById("msg").innerHTML="<p style='color:green'> </p>";
+    
 
-    //controler si les champs ne sont pas vide 
-    for(var i=0;i<=8;i++){
-        if(document.getElementsByTagName("input")[i].value==""){  
- 
-        document.getElementsByClassName("controle")[i].innerHTML='<i class="fas fa-times" style="font-size:23px"></i>';
-        
-            
-        }
-        else
-        document.getElementsByClassName("controle")[i].innerHTML='<i class="fas fa-check" style="color:green;font-size:23px"></i>';
-
-    }
-
-    //contrôle nom et prenom 
+    //contrôle des inputs
+    
+    
+    var controle;
+    //nom
     var nom=document.getElementsByName("nom")[0].value;
-    var prenom=document.getElementsByName("prenom")[0].value;
-
+    if(nom[0]==null){
+        document.getElementById("etat_nom").innerHTML=rouge;
+    }
     for(i=0;i<nom.length;i++){
         if(!(((nom[i]>='A')&&(nom[i]<='Z'))||((nom[i]>='a')&&(nom[i]<='z')))){
-            document.getElementById("etat_nom").innerHTML='<i class="fas fa-times" style="color:red;font-size:23px;"></i><span style="color:red;font-size:25px;font-size:15px;position:relative;bottom:10px">Veuillez saisir que des lettres</span>';
-            return false;
-            
-            
+            document.getElementById("etat_nom").innerHTML=rouge+'<span style="color:red;font-size:25px;font-size:15px;position:relative;bottom:10px">Veuillez saisir que des lettres</span>';
+            controle=false;  
+        }
+        else{
+            document.getElementById("etat_nom").innerHTML=vert;
         }
     }
+
+    //prenom
+    var prenom=document.getElementsByName("prenom")[0].value;
+    if(prenom[0]==null){
+        document.getElementById("etat_prenom").innerHTML=rouge;
+    }
+    for(i=0;i<prenom.length;i++){
+        
+        if(!(((prenom[i]>='A')&&(prenom[i]<='Z'))||((prenom[i]>='a')&&(prenom[i]<='z')))){
+            document.getElementById("etat_prenom").innerHTML=rouge+'<span style="color:red;font-size:25px;font-size:15px;position:relative;bottom:10px">Veuillez saisir que des lettres</span>';
+            controle=false;  
+        }
+        else{
+            document.getElementById("etat_prenom").innerHTML=vert;
+        }
+    }
+
+    //username
+    var username=document.getElementsByName("username")[0].value;
+    if(username[0]==null){
+        document.getElementById("etat_username").innerHTML=rouge;
+    }
+    else{
+        document.getElementById("etat_username").innerHTML=vert;
+    }
+
+    //email
+    var email=document.getElementsByName("email")[0].value;
+    if(email[0]==null){
+        document.getElementById("etat_email").innerHTML=rouge;
+    }
+    else{
+        document.getElementById("etat_email").innerHTML=vert;
+    }
+    
+
+    //mdp
+    var mdp=document.getElementsByName("password")[0].value;
+    
+    if(mdp[0]==null){
+        document.getElementById("etat_mdp").innerHTML=rouge;
+    }
+    else{
+        document.getElementById("etat_mdp").innerHTML=vert;
+    }
+
+
+    //mdp2
+    var mdp2=document.getElementsByName("confirm_password")[0].value;
+    
+    if(mdp2[0]==null){
+        document.getElementById("etat_mdp2").innerHTML=rouge;
+    }
+    else{
+        document.getElementById("etat_mdp2").innerHTML=vert;
+    }
+    
+    //tel
+    var tel=document.getElementsByName("tel")[0].value;
+    if(tel[0]==null){
+        document.getElementById("etat_tel").innerHTML=rouge;
+    }
+    for(i=0;i<tel.length;i++){
+        
+        if(!((tel[i]>='0')&&(tel[i]<='9'))||(tel.length!=10)){
+            document.getElementById("etat_tel").innerHTML=rouge+'<span style="color:red;font-size:25px;font-size:15px;position:relative;bottom:10px">Numéro de téléphone invalide</span>';
+            controle=false;  
+        }
+        else{
+            document.getElementById("etat_tel").innerHTML=vert;
+        }
+    }
+    
+    //sit fam
+    var sit_fam=document.getElementsByName("sit_fam")[0].value;
+    
+    if(sit_fam[0]==null){
+        document.getElementById("etat_sit_fam").innerHTML=rouge;
+    }
+    else{
+        if(!((sit_fam=="Marié")||(sit_fam=="Celibataire"))){
+            document.getElementById("etat_sit_fam").innerHTML=rouge+'<span style="color:red;font-size:25px;font-size:15px;position:relative;bottom:10px">Veuillez choisir parmi les deux choix</span>';
+        }
+        else{
+        document.getElementById("etat_sit_fam").innerHTML=vert;
+        }
+    }
+    
 }
 
 
