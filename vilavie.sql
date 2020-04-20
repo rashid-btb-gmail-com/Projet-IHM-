56b65f2319d9639d23bc112c81ad215ffde471dd
HocineFEDANI <hocinefedani@gmail.com>
2020-04-20 22:53:46 +0100

    Base de donnée.

--- vilavie.sql
+++ vilavie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 20 avr. 2020 à 21:24
+-- Généré le :  lun. 20 avr. 2020 à 21:51
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
