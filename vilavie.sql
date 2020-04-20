a8ba7fff9e3f60cdba499fc0637ba364cb695c68
HocineFEDANI <hocinefedani@gmail.com>
2020-04-20 22:26:36 +0100

    Base de donnée

---
+++ vilavie.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 20 avr. 2020 à 21:24
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `vilavie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `biens`
+--
+
+DROP TABLE IF EXISTS `biens`;
+CREATE TABLE IF NOT EXISTS `biens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `daira` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `commune` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surface` int(11) NOT NULL,
+  `etage` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `biens`
+--
+
+INSERT INTO `biens` (`id`, `titre`, `description`, `daira`, `commune`, `surface`, `etage`) VALUES
+(50, 'grande maison', 'ekekekeke', 'LarbaÃ¢ Nath Irathen', 'AÃ¯t Mahmoud', 1000, 2),
+(48, 'maison 30', 'jhjhjhj', 'LarbaÃ¢ Nath Irathen', 'AÃ¯t Khellili', 123, 2),
+(47, 'maison 2eeleleel', 'leelele', 'Makouda', 'AÃ¯t Aggouacha', 123, 123),
+(49, 'grande maison', 'ekekekeke', 'LarbaÃ¢ Nath Irathen', 'AÃ¯t Mahmoud', 1000, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tel` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sit_fam` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `clients`
+--
+
+INSERT INTO `clients` (`id`, `nom`, `prenom`, `username`, `email`, `password`, `tel`, `sit_fam`) VALUES
+(30, 'fedani', 'test11', 'hoho', 'gogog', '1234', '02131', 'MariÃ©'),
+(25, 'fedani', 'hocine', 'hoho', 'hoho@gmail.com', '123', '055555555', 'MariÃ©'),
+(27, 'fedani', 'hocine', 'hoho', 'hhhhh', '1234', '02131', 'MariÃ©'),
+(28, 'fedani', 'aze', 'aze', 'eeee', '1234', '02135', 'MariÃ©'),
+(31, 'ho', 'ar', 'hoho', 'mmar', '123', '02135', 'Celibataire'),
+(32, 'fedani', 'hocine', 'toto', 'ererer', '1234', '0791913563', 'MariÃ©');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `demande_annonce`
+--
+
+DROP TABLE IF EXISTS `demande_annonce`;
+CREATE TABLE IF NOT EXISTS `demande_annonce` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `daira` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `commune` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surface` bigint(20) NOT NULL,
+  `etage` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rdv_confirmer`
+--
+
+DROP TABLE IF EXISTS `rdv_confirmer`;
+CREATE TABLE IF NOT EXISTS `rdv_confirmer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lieu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_heure` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `rdv_confirmer`
+--
+
+INSERT INTO `rdv_confirmer` (`id`, `client`, `lieu`, `date_heure`) VALUES
+(3, 'hocine  eazea', 'azeaze', '2020-04-18 02:56:00'),
+(4, 'hocine  eazea', 'azeaze', '2020-04-18 02:56:00'),
+(5, 'hohoh', 'agence', '2020-06-17 10:10:00');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
