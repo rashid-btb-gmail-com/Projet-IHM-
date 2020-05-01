ad656ccf1afda62a803ba8b6bd3f11e79c7c2a2a
MassilvaBoutiab <Rashid.btb@gmail.com>
2020-05-01 20:18:40 +0200

    le nouveau footer et reglage de toutes les pages

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,38 @@
 <footer>
-<div class="a_propos">
-    <h5 style="position:absolute;color:#ffffff">A PROPOS DE NOUS :</h5>
-</div>
-
-<div class="social-button">
-    <a href=""><i class="fab fa-facebook-f"></i></a>
-    <a href=""><i class="fab fa-twitter"></i></a>
-    <a href=""><i class="fab fa-instagram"></i></a>
-    <a href=""><i class="fab fa-linkedin"></i></a>
-    <a href=""><i class="fab fa-youtube"></i></a>
-</div>
-</div>
-</footer>
+<footer> 
+       <div class="container ">
+        <div class="foot d-flex justify-content-between">
+         <a href="../index.php" ><img class="logo-foot float-left" src="../images/logo.png" alt="le logo"></a>
+            <div class="d-flex justify-content-between">
+              <div class="onelinkfoot">
+              <h5 class="titrelink">Les biens</h5>
+              <a href="../pages/immobilier.php">Consulter un bien</a>
+              <a href="../pages/deposer_annonce.php">vendre un bien</a>
+              </div>
+              <div class="onelinkfoot">
+              <h5 class="titrelink"> Devenez client</h5>
+              <a href="../pages/inscription.php">Inscrivez-Vous</a>
+              </div>
+              <div class="onelinkfoot">
+              <h5 class="titrelink">Des questions!</h5>
+              <a href="../pages/contact.php">Contactez-Nous</a>
+              </div>
+            </div>
+           <div class="reseign ">
+            <i class="fas fa-phone-square-alt"id="numero"></i><span>026200532</span><br>       
+            <i class="fas fa-map-marker-alt"id="adresse"></i><span class="adres">rue khoudja khaled 
+             batiment 5 tizi-ouzou</span>
+             <div class="social">
+             <h5>rejoignez nous sur<h5>
+             <a href="" title="Notre page facebook" id="fb"class="sns fab fa-facebook-f"></a>
+             <a href=""title="Notre twitter" class="sns fab fa-twitter"></a>
+             <a href="" title="Notre instagram" class="sns fab fa-instagram"></a>
+             <a href="" title="Notre Linked-in" class="sns fab fa-linkedin"></a>
+           </div>
+       </div>
+        </div>
+        
+       </div>
+     </footer> 
 <script src="./js/jquery"></script>
+
