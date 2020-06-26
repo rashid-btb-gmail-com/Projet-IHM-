464daf1cbd3607e09dcbe7cd6704712f6082b05a
MassilvaBoutiab <Rashid.btb@gmail.com>
2020-06-27 00:33:59 +0200

    ajout de initial

--- pages/profile.php
+++ pages/profile.php
@@ -27,8 +27,42 @@
 <div class="container-fluid d-flex justify-content-center" id="profile">
     <section class="labox"> 
         <div class="d-flexbox flexrow">
-            <div class=""></div>
-            <div></div>
+            <div class=" d-flex justify-content-center" >
+                <div class="proinit">
+                    <?php 
+                    $nom=$_SESSION["nom"];
+                    $prenom=$_SESSION["prenom"]; 
+                    echo('<h1 id="initial">'.$prenom[0].$nom[0].'</h1>');                                  
+                    ?>
+                </div>
+            </div>
+            <div class=" d-flex ">
+                
+                <div>
+                <?php 
+                    $proclient=$db->query("SELECT * FROM clients WHERE id=".$_SESSION["id"]."");
+                    $profile = $proclient->fetch();
+                    echo('
+                    
+                 <div class="input-group mb-3">
+                    <input type="text" name="nom" class="form-control input_user" placeholder="votre nom" value="'.$profile["nom"].'">
+
+                 </div>
+                 <div class="input-group mb-3">
+                    <input type="text" name="prenom" class="form-control input_user" placeholder="votre prenom" value="'.$profile["prenom"].'">
+
+                 </div>                                
+                 <div class="input-group mb-3">
+                   <input type="tel" name="tel" class="form-control input_user" placeholder="numero de telephone" value="'.$profile["tel"].'">
+                 </div> 
+                 <div class="input-group mb-3">
+                    <input type="email" name="mail" class="form-control input_user" placeholder="votre email" value="'.$profile["email"].'">
+
+                 </div>     
+                 ');
+                ?>
+                </div>
+            </div>
             <div></div>
         </div>
     </section>
